f70260b2ee1e954ef832d15bb160005e33ea397d
Eduardo Vicente <eduardomcv42@gmail.com>
2022-01-21 17:47:55 +0000

    add rollDiceCode

---
+++ src/rollDiceCode.ts
@@ -0,0 +1,16 @@
+import { rollDice } from './rollDice';
+
+const regex: RegExp = /(\d+)?d(\d+)/gi;
+
+export function rollDiceCode(diceCode: string): number[] {
+  const result = regex.exec(diceCode);
+
+  if (result === null) {
+    throw new Error('Invalid dice code.');
+  }
+
+  const dice = Number(result[1]);
+  const sides = Number(result[2]);
+
+  return rollDice(dice, sides);
+}
