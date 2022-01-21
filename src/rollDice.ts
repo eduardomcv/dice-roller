c73375d476467f9d7b4af4c096f4d1be2300bb65
Eduardo Vicente <eduardomcv42@gmail.com>
2022-01-21 17:12:25 +0000

    add throws

--- src/rollDice.ts
+++ src/rollDice.ts
@@ -1,4 +1,12 @@
 export function rollDice(dice: number, sides: number): number[] {
+  if (!Number.isInteger(dice)) {
+    throw new Error('dice is not an integer');
+  }
+
+  if (!Number.isInteger(sides)) {
+    throw new Error('sides is not an integer');
+  }
+
   const results = new Array(dice);
 
   for (let i = 0; i < dice; i += 1) {
